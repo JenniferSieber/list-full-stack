62fdd29a2a9b173cc16823940dd9c508f7ce47c3
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2024-03-19 16:32:30 -0600

    Add files via upload

---
+++ routes/itemRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const Item = require('../models/Item')
+const { getItems, createItem, updateItem, deleteItem } = require('../controllers/itemController')
+
+router.get('/', (req, res) => {
+  res.render('index')
+})
+router.get('/item', getItems)
+router.post('/item', createItem)
+router.post('/item/update/:id', updateItem)
+router.delete('/item/delete/:id', deleteItem)
+
+
+module.exports = router
